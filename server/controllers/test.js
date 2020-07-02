dbeb4f8fc018e35f28f254e6be86f01f8628e0b1
myaws1337 <myaws1337@gmail.com>
2020-07-02 19:24:56 -0400

    testing

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -1,4 +1,11 @@
 /**
  * http://usejsdoc.org/
  */
-connection.query("SELECT * FROM product WHERE prod = '"+ req.body.prod +"' AND product_name = '"+ req.body.product_name +"'",function(error, results){});
+connection.query(
+  "SELECT * FROM product WHERE prod = '" +
+    req.body.prod +
+    "' AND product_name = '" +
+    req.body.product_name2 +
+    "'",
+  function (error, results) {}
+);
