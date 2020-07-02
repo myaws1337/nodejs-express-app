f5b3c261a2fe7d92ddfd178f1dd5539c0273b48d
myaws1337 <myaws1337@gmail.com>
2020-07-02 17:09:30 -0400

    tested

--- server/controllers/test.js
+++ server/controllers/test.js
@@ -1,4 +1,4 @@
 /**
  * http://usejsdoc.org/
  */
-connection.query("SELECT * FROM bank_accounts WHERE dob = '"+ req.body.dob +"' AND bank_account = '"+ req.body.account_number +"'",function(error, results){});
+connection.query("SELECT * FROM bank_accounts WHERE dob = '"+ req.body.dob +"' AND bank_account = '"+ req.body.account_number2 +"'",function(error, results){});
